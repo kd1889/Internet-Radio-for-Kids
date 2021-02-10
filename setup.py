ba6418f336fa9f61da1d8ff96dce58b5595a3c0c
Samuel Boahen <sb6331@nyu.edu>
2021-02-10 13:24:00 -0500

    Added stub software library, see #2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stub',
+      version='1.0',
+      description='CompE Stub Library',
+      author='Samuel Boahen',
+      author_email='sb6331@nyu.edu',
+      url='https://github.com/kd1889/Internet-Radio-for-Kids.git',
+      py_modules=['stub'],
+     )
+
