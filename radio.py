4884b7ed6020004a4948dc917573b285b9c3270e
Samuel Boahen <sb6331@nyu.edu>
2021-02-17 13:45:02 -0500

    Adds testing for led on off operation

--- radio.py
+++ radio.py
@@ -1,3 +1,15 @@
 def info():  
     '''Prints a basic library description'''
     print("Software library for the stub project.")
+
+
+def is_led_on(led_pin):
+    """
+    returns True if led at with pin number led_pin is on 
+    else returns false
+    """
+
+    return True
+
+
+
--- stub.py
+++
@@ -1,3 +0,0 @@
-def info():  
-    '''Prints a basic library description'''
-    print("Software library for the stub project.")
