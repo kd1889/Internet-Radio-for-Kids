4bdf183a3617b95472d0786c357ac663288f147d
Samuel Boahen <sb6331@nyu.edu>
2021-02-17 13:31:43 -0500

    Changes file and module naming

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='stub',
+setup(name='radio',
       version='1.0',
       description='CompE Stub Library',
       author='Samuel Boahen',
       author_email='sb6331@nyu.edu',
       url='https://github.com/kd1889/Internet-Radio-for-Kids.git',
-      py_modules=['stub'],
+      py_modules=['radio'],
      )
 
