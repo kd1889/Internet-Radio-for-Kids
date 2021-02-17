fe1490376dc5a3cc7a163ef5cee45d49cb7be0e9
Samuel Boahen <sb6331@nyu.edu>
2021-02-17 13:53:48 -0500

    Adds test for button. Pressed condition

--- radio.py
+++ radio.py
@@ -1,15 +1,21 @@
-def info():  
+def info():
     '''Prints a basic library description'''
     print("Software library for the stub project.")
 
 
 def is_led_on(led_pin):
     """
-    returns True if led at with pin number led_pin is on 
+    returns True if led at pin number led_pin is on 
     else returns false
     """
 
     return True
 
+def is_button_pressed(pin):
+    """
+    returns True if button at pin number pin is on
+    else returns false
+    """
 
+    return True
 
