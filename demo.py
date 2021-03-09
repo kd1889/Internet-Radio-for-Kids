62107a21be1eb2e10e1ffe2e5fa6fe7239a6bcbd
kd1889 <kd1889@nyu.edu>
2021-03-09 18:49:19 -0500

    Updated the toggle_display() and send_to_screen functions to test whether the screen works, which it does, #5

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 import radio
+import time;
+
 print("Testing is_led_on");
 
 print(radio.is_led_on(0));
@@ -6,14 +8,17 @@ print(radio.is_led_on(0));
 print("Testing is_button_pressed");
 print(radio.is_button_pressed(0));
 
-print("Testing toggle_display with both possible inputs");
-radio.toggle_display(0)
-radio.toggle_display(1)
-
 print("Testing send_data_to_screen")
-radio.send_data_to_screen("My name is Kunj")
+radio.setup_LCD();
+time.sleep(0.005);
+radio.send_data_to_screen("kd1889")
+
+print("Testing toggle display with both inputs");
+radio.toggle_display(0);
+time.sleep(1);
+radio.toggle_display(1);
+time.sleep(0);
 
 print("Testing play_sound");
 radio.play_sound("'Hello' by Lionel Richie")
-
 
