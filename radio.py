06f255223d9d0f14cca4571ecaa9512a22c75894
Samuel Boahen <sb6331@nyu.edu>
2021-03-15 07:05:59 +0000

    updates codebase structure

--- radio.py
+++ radio.py
@@ -1,32 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+from resources.utils import (
+    LCD_MAP,
+    LCD_COMMAND,
+    TIMING,
+)
+
 GPIO.setwarnings(False);
 GPIO.setmode(GPIO.BCM);
-#GPIO TO LCD MAPPING
-LCD_RS = 5;
-LCD_E = 13
-LCD_RW = 6;
-LCD_D4 = 23;
-LCD_D5 = 22;
-LCD_D6 = 27;
-LCD_D7 = 17;
 
 #RS PIN STATES FOR CHARACTER AND COMMAND MODE
 LCD_CHR = GPIO.HIGH;
 LCD_CMD = GPIO.LOW;
 
-#IMPORTANT COMMANDS FOR 4BIT
-LCD_CLEAR = [0,0,0,0,0,0,0,1];
-LCD_D_OFF = [0,0,0,0,1,0,0,0];
-LCD_4BIT1 = [0,0,1,1,0,0,1,1];
-LCD_4BIT2 = [0,0,1,1,0,0,1,0];
-LCD_ON_NC = [0,0,0,0,1,1,0,0];
-LCD_ENTRY = [0,0,0,0,0,1,1,0];
-
-#TIMING CONSTANTS
-E_PULSE = 0.0005;
-E_DELAY = 0.0005;
 
 def info():
     '''Prints a basic library description'''
@@ -54,52 +41,61 @@ def char_to_arr(c):
     return [int(b) for b in format(ord(c), '08b')];
 
 def write_arr_4bits(bits, mode, debug=True):
-    pins = [LCD_D7, LCD_D6, LCD_D5, LCD_D4];
-    GPIO.setup(LCD_RS, GPIO.OUT); 
-    GPIO.output(LCD_RS, mode);
+    pins = [LCD_MAP["LCD_D7"], LCD_MAP["LCD_D6"], LCD_MAP["LCD_D5"], LCD_MAP["LCD_D4"]];
+    GPIO.setup(LCD_MAP["LCD_RS"], GPIO.OUT);
+    GPIO.output(LCD_MAP["LCD_RS"], mode);
 
     for p, b in zip(pins, bits[:4]):
        GPIO.setup(p, GPIO.OUT);
        GPIO.output(p,b);
     
-    time.sleep(E_DELAY);
-    GPIO.output(LCD_E, GPIO.HIGH);
-    time.sleep(E_PULSE);
-    GPIO.output(LCD_E, GPIO.LOW);
-    time.sleep(E_DELAY);
+    time.sleep(TIMING["E_DELAY"]);
+    GPIO.output(LCD_MAP["LCD_E"], GPIO.HIGH);
+    time.sleep(TIMING["E_PULSE"]);
+    GPIO.output(LCD_MAP["LCD_E"], GPIO.LOW);
+    time.sleep(TIMING["E_DELAY"]);
 
     for p, b in zip(pins, bits[4:]):
         GPIO.setup(p, GPIO.OUT);
         GPIO.output(p, b);
     
-    time.sleep(E_DELAY);
-    GPIO.output(LCD_E, GPIO.HIGH);
-    time.sleep(E_PULSE);
-    GPIO.output(LCD_E, GPIO.LOW);
-    time.sleep(E_DELAY);
+    time.sleep(TIMING["E_DELAY"]);
+    GPIO.output(LCD_MAP["LCD_E"], GPIO.HIGH);
+    time.sleep(TIMING["E_PULSE"]);
+    GPIO.output(LCD_MAP["LCD_E"], GPIO.LOW);
+    time.sleep(TIMING["E_DELAY"]);
 
     for p in pins:
         GPIO.output(p, GPIO.LOW);
 def toggle_display(state):
     if (state == 0):
-        write_arr_4bits(LCD_D_OFF, LCD_CMD);
+        write_arr_4bits(LCD_COMMAND["LCD_D_OFF"], LCD_CMD);
         state_str = "OFF"
     else:
-        write_arr_4bits(LCD_ON_NC, LCD_CMD);
+        write_arr_4bits(LCD_COMMAND["LCD_ON_NC"], LCD_CMD);
         state_str = "ON";
     print("LCD has been toggled " + state_str);
 
 def setup_LCD():
-    LCD_PINS = [LCD_RS, LCD_RW, LCD_E, LCD_D7, LCD_D6, LCD_D5, LCD_D4];
+    LCD_PINS = [
+                 LCD_MAP["LCD_RS"],
+                 LCD_MAP["LCD_RW"],
+                 LCD_MAP["LCD_E"],
+                 LCD_MAP["LCD_D7"],
+                 LCD_MAP["LCD_D6"],
+                 LCD_MAP["LCD_D5"],
+                 LCD_MAP["LCD_D4"],
+    ]
+
     for p in LCD_PINS:
         GPIO.setup(p, GPIO.OUT);
-    GPIO.output(LCD_RW, GPIO.LOW);
+    GPIO.output(LCD_MAP["LCD_RW"], GPIO.LOW);
 
-    write_arr_4bits(LCD_4BIT1, LCD_CMD);
-    write_arr_4bits(LCD_4BIT2, LCD_CMD);
-    write_arr_4bits(LCD_ON_NC, LCD_CMD);
-    write_arr_4bits(LCD_ENTRY, LCD_CMD);
-    write_arr_4bits(LCD_CLEAR, LCD_CMD);
+    write_arr_4bits(LCD_COMMAND["LCD_4BIT1"], LCD_CMD);
+    write_arr_4bits(LCD_COMMAND["LCD_4BIT2"], LCD_CMD);
+    write_arr_4bits(LCD_COMMAND["LCD_ON_NC"], LCD_CMD);
+    write_arr_4bits(LCD_COMMAND["LCD_ENTRY"], LCD_CMD);
+    write_arr_4bits(LCD_COMMAND["LCD_CLEAR"], LCD_CMD);
    
 def send_data_to_screen(text):
     """
---
+++ resources/utils.py
@@ -0,0 +1,26 @@
+# GPIO TO LCD MAPPING
+LCD_MAP = {
+    "LCD_RS": 5,
+    "LCD_E": 13,
+    "LCD_RW": 6,
+    "LCD_D4": 23,
+    "LCD_D5": 22,
+    "LCD_D6": 27,
+    "LCD_D7": 17,
+}
+
+# IMPORTANT COMMANDS FOR 4BIT
+LCD_COMMAND = {
+    "LCD_CLEAR": [0,0,0,0,0,0,0,1],
+    "LCD_D_OFF": [0,0,0,0,1,0,0,0],
+    "LCD_4BIT1": [0,0,1,1,0,0,1,1],
+    "LCD_4BIT2": [0,0,1,1,0,0,1,0],
+    "LCD_ON_NC": [0,0,0,0,1,1,0,0],
+    "LCD_ENTRY": [0,0,0,0,0,1,1,0],
+}
+
+# TIMING CONSTANTS
+TIMING = {
+    "E_PULSE": 0.0005,
+    "E_DELAY": 0.0005,
+}
