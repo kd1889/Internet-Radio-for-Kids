295bb43075dc68b8cbf0ebb5a461e79197164687
Samuel Boahen <sb6331@nyu.edu>
2021-04-18 05:36:31 -0400

    Adds prototype for MusicPlayer

--- button_lcd_demo.py
+++ button_lcd_demo.py
@@ -3,10 +3,66 @@ import signal
 import sys
 import RPi.GPIO as GPIO
 from resources.utils import BUTTON, LCD_COMMAND
+import time
+import threading
 
 GPIO.setmode(GPIO.BCM)
 LCD_CMD = GPIO.LOW
 radio.setup_pins()
+exit_event = threading.Event() # exit_event for background musicPlayer
+
+class MusicPlayer(threading.Thread):
+
+    def __init__(self):
+        super().__init__()
+        self.trackList = ["Jack", "Join", "Monroe", "Emily"]
+        self.index = 0 # for track number in trackList
+        self.isPlaying = False # attribute is set to true when track is playing
+        self.changeTrack = False # set to true when changing track
+
+        # threading to keep play running at all times in background
+        thread = threading.Thread(target=self.play, args=())
+        thread.daemon = True
+        thread.start()
+
+
+    def play(self):
+
+        while True:
+            if exit_event.is_set():
+                break # kill background task play at the end when ctrl + c hit
+            if self.changeTrack:
+
+                self.play_track(self.index)
+                self.changeTrack = False
+            time.sleep(1)
+
+    def play_track(self, trackNumber=0):
+        """
+        play track should be updated with actual code for playing station or
+        track
+        """
+
+        self.isPlaying = True
+
+        for i in range(5):
+            if not self.isPlaying:
+                break
+            print("playing track: " + self.trackList[trackNumber])
+            time.sleep(1)
+
+        self.isPlaying = False # done playing music
+
+    def kill_music(self):
+        self.isPlaying = False
+
+    def play_next_track(self, trackNumber):
+        self.kill_music()
+        # sleep time here must be greater than that in play to ensure effect
+        # of kill_music and change track
+        time.sleep(1.1)
+        self.index = trackNumber
+        self.changeTrack = True
 
 class Page:
 
@@ -29,6 +85,14 @@ class Page:
         """
         self.display_features()
 
+    def exit_actions(self, actionNumber=0):
+        """
+        actionNumber is the option number on the screen
+        performs default actions when page is being exited
+        this method may be overriden in subclasses
+        """
+        return
+
 class PlaySomething(Page):
     FEATURES = ["Radio 1", "Playlist 1", "More"]
 
@@ -39,6 +103,9 @@ class PlaySomething(Page):
         self.playlist_number = 0 # playlist number on screen - 1
         self.number_of_playlists = 4
         self.number_of_stations = 4
+
+        self.musicPlayer = MusicPlayer()
+        self.play_track()
 
     @staticmethod
     def increment_index(inc, maxIndex):
@@ -65,7 +132,16 @@ class PlaySomething(Page):
     def perform_actions(self, actionNumber=0):
         if actionNumber == 3:
             self.next_option()
+
         self.display_features()
+
+    def exit_actions(self, actionNumber=0):
+        if actionNumber == 2: # playing playlist
+            self.play_track(self.playlist_number)
+
+    def play_track(self, trackNumber=0):
+
+        self.musicPlayer.play_next_track(trackNumber)
 
 class HomePage(Page):
     FEATURES = ["Play Something", "Change User", "Setup"]
@@ -94,6 +170,7 @@ class Book:
         }
 
         self.currPageName = "hp"
+        self.currPage = self.home
 
     def input(self, pin):
 
@@ -125,13 +202,15 @@ class Book:
                 buttonNumber = 5
 
         if output is not None:
-            currPage, self.currPageName = output
-            currPage.perform_actions(buttonNumber)
+            self.currPage.exit_actions(buttonNumber)
+            self.currPage, self.currPageName = output
+            self.currPage.perform_actions(buttonNumber)
 
 curr = Book()
 
 
 def signal_handler(sig, frame):
+    exit_event.set()
     GPIO.cleanup()
     sys.exit(0)
 
