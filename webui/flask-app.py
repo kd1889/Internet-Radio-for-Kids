0a91214a5688856c5abcbfa34a489af676577194
kd1889 <kd1889@nyu.edu>
2021-04-28 13:44:24 -0400

    Fixed issues with proper website dislay. Started to create configuration files using YAML, #19, #17, #8, #18

--- webui/flask-app.py
+++ webui/flask-app.py
@@ -1,27 +1,18 @@
 from flask import Flask, redirect, render_template
+import yaml
 
-app = Flask(__name__, static_folder='~/curr_project/Internet-Radio-for-Kids/webui/assets')
+app = Flask(__name__, static_folder='assets')
 
-
-stations = [];
-file = open("stations.txt", 'r');
-def fill_stations(file):
-    for line in file:
-       name = line[:line.find("|")];
-       line = line[line.find("|")+1:];
-       url = line[:line.find("|")];
-       line = line[line.find("|")+1:];
-       state = line;
-       stations.append({'name':name, 'url':url, 'state':state});
-
+file = open("radio.yaml", 'r');
+stations = yaml.safe_load(file)['stations'];
 @app.route("/")
 def hello():
-    fill_stations(file);
-    return render_template('index.html', stations=stations);
+    return render_template('index.html');
 
 @app.route("/radio.html")
 def radio():
-    return render_template('radio.html');
+#    fill_stations(file)
+    return render_template('radio.html', stations=stations);
 
 @app.route("/control.html")
 def control():
