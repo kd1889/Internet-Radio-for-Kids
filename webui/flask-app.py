b1fafd5a30f369f6eaf6d773afd218f2435976ce
Samuel Boahen <sb6331@nyu.edu>
2021-04-28 05:04:11 -0400

    Updates web app with upload option

--- webui/flask-app.py
+++ webui/flask-app.py
@@ -1,5 +1,8 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, request, redirect, render_template
+import os
 
+MUSIC_UPLOAD_PATH = "/home/pi/Desktop/new/Internet-Radio-for-Kids/uploads"
+ALLOWED_MUSIC_EXTENSIONS = ["MP3", "WAV"]
 app = Flask(__name__, static_folder='~/curr_project/Internet-Radio-for-Kids/webui/assets')
 
 
@@ -31,6 +34,35 @@ def control():
 def music():
     return render_template('music.html');
 
+def valid_extension(file_name):
+
+    if "." not in file_name:
+        return False
+
+    ext = file_name.split(".")[-1]
+
+    return ext.upper() in ALLOWED_MUSIC_EXTENSIONS
+
+@app.route("/upload-music", methods=["GET", "POST"])
+def upload_music():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            music = request.files["music"]
+
+            if not valid_extension(music.filename):
+                print("This file type is not allowed")
+                return redirect(request.url)
+
+            music.save(os.path.join(MUSIC_UPLOAD_PATH, music.filename))
+
+            print("music saved")
+
+            return redirect(request.url)
+
+    return render_template('upload_music.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
