0ec0d53ca9c54ba0f478a325c397296e1a2b9a10
Samuel Boahen <sb6331@nyu.edu>
2021-05-03 00:16:15 -0400

    Adds page for parental control settings

--- webui/flask-app.py
+++ webui/flask-app.py
@@ -34,6 +34,62 @@ def control():
 def music():
     return render_template('music.html');
 
+# defualt values at the start of the program
+CONFIG = {
+          "textbox": {
+                      "start_time": 1,
+                      "end_time": 4,
+                      "max_playtime": 5
+          },
+          "drop_down": {
+                        "pc_status": [ # [name, is_selected]
+                                      ["Enable", False],
+                                      ["Disable", True]
+                        ],
+                        "add_music_radio": [
+                                            ["Enable_Session", False],
+                                            ["Enable_Always", False],
+                                            ["Disable", True]
+                        ]
+         }
+}
+
+def update_config(values):
+    CONFIG["textbox"]["start_time"] = values["start_time"]
+    CONFIG["textbox"]["end_time"] = values["end_time"]
+    CONFIG["textbox"]["max_playtime"] = values["max_playtime"]
+    newPcStatus = values["pc_status"]
+    newMusicRadioStatus = values["add_music_radio"]
+
+    for statusOption in CONFIG["drop_down"]["pc_status"]:
+        if statusOption[0] == newPcStatus:
+            statusOption[1] = True
+        else:
+            statusOption[1] = False
+
+    for statusOption in CONFIG["drop_down"]["add_music_radio"]:
+        if statusOption[0] == newMusicRadioStatus:
+            statusOption[1] = True
+        else:
+            statusOption[1] = False
+
+@app.route("/parental-control", methods=["GET", "POST"])
+def parental_control():
+
+    if request.method == "POST":
+        if request.form:
+
+            print("pc_status:",request.form.get("pc_status"))
+            print("max_playtime:", request.form.get("max_playtime"))
+            print("start_time:", request.form.get("start_time"))
+            print("end_time:", request.form.get("end_time"))
+            print("add_music_radio:", request.form.get("add_music_radio"))
+
+            update_config(request.form)
+            return redirect(request.url)
+
+    return render_template('parental.html', config=CONFIG)
+
 def valid_extension(file_name):
 
     if "." not in file_name:
